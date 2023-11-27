4a9e624ac42214899074b562f5ead91bf5429d3e
Praveen Dwivedi <121290329+praveendwievedi@users.noreply.github.com>
2023-11-27 12:17:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+let btn = document.getElementById('btn');
+let para = document.getElementById('click');
+
+btn.addEventListener("click",()=>{
+    if(para.style.display !== "none"){
+        para.style.display = "none";
+    }else{
+        para.style.display = "block";
+    }
+})
+//question2//
+let btn1 = document.getElementById('btn1');
+let para1 = document.getElementById('click1');
+btn1.addEventListener("click",()=>{
+    if(para1.style.display !== "none"){
+        para1.style.display = "none";
+    }else{
+        para1.style.display = "block";
+    }
+})
+//question 3//
+let btn2 = document.getElementById('btn2');
+let para2 = document.getElementById('click2');
+btn2.addEventListener("click",()=>{
+    if(para2.style.display !== "none"){
+        para2.style.display = "none";
+    }else{
+        para2.style.display = "block";
+    }
+})
+//question 4//
+let btn3 = document.getElementById('btn3');
+let para3 = document.getElementById('click3');
+btn3.addEventListener("click",()=>{
+    if(para3.style.display !== "none"){
+        para3.style.display = "none";
+    }else{
+        para3.style.display = "block";
+    }
+})
+//question5//
+let btn4 = document.getElementById('btn4');
+let para4 = document.getElementById('click4');
+btn4.addEventListener("click",()=>{
+    if(para4.style.display !== "none"){
+        para4.style.display = "none";
+    }else{
+        para4.style.display = "block";
+    }
+})
+//question6//
+let btn5 = document.getElementById('btn5');
+let para5 = document.getElementById('click5');
+btn5.addEventListener("click",()=>{
+    if(para5.style.display !== "none"){
+        para5.style.display = "none";
+    }else{
+        para5.style.display = "block";
+    }
+})
